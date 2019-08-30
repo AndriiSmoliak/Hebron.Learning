d40363a077c68742cf262efe149c480103b62130
SmolyakAndriy <smolyakandriy777@gmail.com>
2019-08-30 11:20:11 +0300

    HW4 function isPrime versional#2

--- HW4/src/isPrime.js
+++ HW4/src/isPrime.js
@@ -1,15 +1,23 @@
-function isPrime(num){
-    if(num === 1) return true;
-    let a = num / 2;
-    let i = 2;
-    while(i <= a) {
-        if(num % i === 0) {
-            return false
-        } else {
-            i++;
-        }
+// function isPrime(num){
+//     if(num === 1) return true;
+//     let a = num / 2;
+//     let i = 2;
+//     while(i <= a) {
+//         if(num % i === 0) {
+//             return false
+//         } else {
+//             i++;
+//         }
+//     }
+//     return true;
+// }
+// console.log(isPrime(32));
+
+function isPrime(num) {
+    if (num % 2 === 0 || num % 2 ===0){
+        return false;
+    } else {
+        return true;
     }
-    return true;
 }
 console.log(isPrime(32));
-
