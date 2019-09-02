0f73385aba0999e3d8f280e6fff9fa9666602545
SmolyakAndriy <smolyakandriy777@gmail.com>
2019-09-02 23:30:37 +0300

    fixing isPrime

--- HW4/src/isPrime.js
+++ HW4/src/isPrime.js
@@ -1,20 +1,5 @@
-// function isPrime(num){
-//     if(num === 1) return true;
-//     let a = num / 2;
-//     let i = 2;
-//     while(i <= a) {
-//         if(num % i === 0) {
-//             return false
-//         } else {
-//             i++;
-//         }
-//     }
-//     return true;
-// }
-// console.log(isPrime(32));
-
 function isPrime(num) {
-    if (num % 2 === 0 || num % 2 ===0){
+    if (num % 2 === 0 || num % 3 === 0){
         return false;
     } else {
         return true;
